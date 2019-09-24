1fd8edff0a2e9a85f672a05747661a2e0b1fb0a5
sloothes <sloothes.com@gmail.com>
2019-09-24 23:29:53 +0300

    Update

--- r78/editor/js/libs/app.js
+++ r78/editor/js/libs/app.js
@@ -66,18 +66,6 @@ var APP = {
 			this.setScene( loader.parse( json.scene ) );
 			this.setCamera( loader.parse( json.camera ) );
 
-		//  Joystick controls.
-
-			var joysticControls1 = document.createElement( "div" );
-			var joysticControls2 = document.createElement( "div" );
-			joysticControls1.id = "joystick-controls-1";
-			joysticControls2.id = "joystick-controls-2";
-			joysticControls1.classList.add("joystick-controls");
-			joysticControls2.classList.add("joystick-controls");
-
-			this.dom.appendChild( joysticControls1 );
-			this.dom.appendChild( joysticControls2 );
-
     /*
         //  Player editor controls (at runtime).
 		//	always "after" setCamera(); important!

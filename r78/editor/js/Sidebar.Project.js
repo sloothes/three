57c60efedcec8c8bdd6169103460ab94470ed721
sloothes <sloothes.com@gmail.com>
2019-10-22 06:09:55 +0300

    Update Sidebar.Project.js

--- r78/editor/js/Sidebar.Project.js
+++ r78/editor/js/Sidebar.Project.js
@@ -577,6 +577,26 @@ Sidebar.Project = function ( editor ) {
 
 	}
 
+	var link = document.createElement( "a" );
+	link.style.display = "none";
+	document.body.appendChild( link ); // Firefox workaround, see #6594
+
+	function save( blob, filename ) {
+
+		link.href = URL.createObjectURL( blob );
+		link.download = filename || "data.json";
+		link.click();
+
+	//	URL.revokeObjectURL( url ); breaks Firefox...
+
+	}
+
+	function saveString( text, filename ) {
+
+		save( new Blob( [ text ], { type: "text/plain" } ), filename );
+
+	}
+
 /*
 	function uploadDataURL(data, type, name){
 
